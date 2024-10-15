247c327a809772fdc3d04ab4087a1aee722c4f2b
Ashley Stewart <a.stewart.au@gmail.com>
2024-10-15 16:53:06 -0500

    Fix datatype_tags for t2starw

--- bids-walker.js
+++ bids-walker.js
@@ -1286,23 +1286,32 @@ exports.walk = (root, cb)=>{
     }
 
     function handle_anat_t2starw(derivatives, parent_sidecar, dir, fileinfo, cb) {
-        // load sidecar
+        // Load sidecar
         let json_filename = fileinfo._filename.replace(/\.nii(\.gz)?$/, ".json");
         let sidecar = {};
         Object.assign(sidecar, parent_sidecar[json_filename]);
         Object.assign(sidecar, get_sidecar_from_fileinfo(dir, fileinfo, json_filename));
-        
-        // define brainlife dataset
+    
+        // Get all tags
+        let all_tags = get_tags(fileinfo);
+    
+        // Filter tags for datatype_tags (only "part-mag" and/or "part-phase")
+        let datatype_tags = all_tags.filter(tag => tag === "part-mag" || tag === "part-phase");
+    
+        // Filter tags for tags (exclude "part-mag" and "part-phase")
+        let tags = all_tags.filter(tag => tag !== "part-mag" && tag !== "part-phase");
+    
+        // Define brainlife dataset
         let dataset = {
             datatype: "neuro/anat/t2starw",
             desc: fileinfo._fullname,
-            datatype_tags: [],
-            tags: get_tags(fileinfo),
+            datatype_tags: datatype_tags,
+            tags: tags,
             meta: Object.assign(sidecar, get_meta(fileinfo)),
-        }
-        
-        // assign files
-        let files = { 't2starw.nii' : dir + "/" + fileinfo._fullname };
+        };
+    
+        // Assign files
+        let files = { 't2starw.nii': dir + "/" + fileinfo._fullname };
         bids.datasets.push({ dataset, files });
         cb();
     }
